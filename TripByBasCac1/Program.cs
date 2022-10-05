a147d8fa6c67d9bae1b7c27c3a0f3b959f978a1a
С - 15 < - 15@DESKTOP-S44TEQV>
2022-10-05 21:20:03 +0500

    Добавьте файлы проекта.

---
+++ TripByBasCac1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TripByBusCacl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var TimeTable = new TextIO("Bus.txt");
+            var _timeTable = new List<string>();
+            _timeTable = TimeTable.ReadString();
+            //var regex = new Regex(@"\d{2}:\d{2}");
+            //MatchCollection _match;
+            var _text = new TextAnalayser();
+            MatchCollection tmp;
+            string _pattern = @"Химмаш\s*\d{2}:\d{2}", _pattern002 = @"\d{2}:\d{2}";
+            foreach (var item in _timeTable)
+            {
+                //if(item.Contains("Химмаш"))
+                //Console.WriteLine(regex.Matches(item)[0].Value.ToString());
+                _text.Search(item, _pattern, out tmp);
+                _text.Search(tmp[0].ToString(), _pattern002, out tmp);
+                |Console.WriteLine(tmp[0]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TripByBasCac1/TextIO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TripByBusCacl
+{
+    public class TextIO
+    {
+        public string Name { get; }
+        public TextIO()
+        {
+            Console.WriteLine("Задайте имя файла");
+            Name = Console.ReadLine();
+        }
+        public TextIO(string name)
+        {
+            Name = name;
+        }
+
+        public void WriteToFile(string _text)
+        {
+            var sw = new StreamWriter(this.Name, true);
+            sw.WriteLine(_text);
+            sw.Close();
+        }
+        public List<string> ReadString()
+        {
+            var sr = new StreamReader(this.Name);
+            var _arr = new List<string>();
+            string _tmp;
+            while ((_tmp = sr.ReadLine()) != null)
+            {
+                _arr.Add(_tmp);
+            }
+            sr.Close();
+            return _arr;
+        }
+    }
+
+    public class TextAnalayser
+    {
+        public string firstargAndOperation { get; }
+        public string numbers { get; }
+        public string operation { get; }
+        public string secondargAndOperation { get; }
+        public TextAnalayser()
+        {
+            this.firstargAndOperation = @"\d+\s*(\+|\-|\*|\/)";
+            this.numbers = @"\d+";
+            this.operation = @"\+|\-|\*|\/";
+            this.secondargAndOperation = @"(\+|\-|\*|\/)\s*\d+";
+        }
+        public void Search(string _input, string _pattern, out MatchCollection _matches)
+        {
+            var _regex = new Regex(_pattern);
+            _matches = _regex.Matches(_input);
+        }
+    }
+}
